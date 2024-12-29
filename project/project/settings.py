8346dfa2179e58b5d85a6643888b7cbb98bb0731
Xhector <raulromerog002@gmail.com>
2024-12-29 13:23:05 -0500

    Primer Commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -141,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import django_heroku
+django_heroku.settings(locals())
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
